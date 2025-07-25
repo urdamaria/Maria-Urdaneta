ade0c38e0bd86378b5ea141f623751029db08850
urdamaria <urdanetamaria166@gmail.com>
2025-07-25 13:58:11 -0400

    tarea4

---
+++ modulo_1/Python/Clase4.py
@@ -0,0 +1,34 @@
+""""
+diccionario = {
+    "nombre" : "Ana",
+    "edad" : 20,
+    "carrera" : "Ingenieria"
+}
+
+print ( diccionario ["carrera"])
+diccionario["manzana"] = 0.5
+diccionario["banana"] = 0.3
+diccionario["banana"] = 0.4
+"""""
+"""""
+diccionario = {
+    "palabra" : "banana",
+    "b" : 1, "a" : 2, "n" : 3
+}
+
+print("\nPares clave-valor del diccionario:", diccionario.items())
+for clave in diccionario:
+    print(f"\nClave: {clave}, Valor: {diccionario[clave]}")
+   
+ """
+
+"""
+diccionario_cap =  {
+    "francia" : "paris",
+    "italia" : "Roma", 
+    "españa" : "madrid"
+}
+for clave, valor in diccionario_cap.items():
+    print(f"\nLa capital de {clave}, es {valor}")
+
+"""
