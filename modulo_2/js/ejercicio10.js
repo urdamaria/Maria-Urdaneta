f40ec15266b69286a3ee85dad273470679bf7362
urdamaria <urdanetamaria166@gmail.com>
2025-08-02 12:57:04 -0400

    actuuuuuu

--- modulo_2/js/ejercicio10.js
+++ modulo_2/js/ejercicio10.js
@@ -1,10 +1,10 @@
 let monto = 120;
 let precioFinal;
 
-if (montoCompra > 100){
+if (monto > 100){
     precioFinal = monto * 0.80;
     console.log ("Felicidades, Tienes un 20% de descuento.");
-} else if (montoCompra > 50){
+} else if (monto > 50){
     precioFinal = monto * 0.90;
     console.log ("Tienes un 10% de descuento");
 } else {
@@ -12,4 +12,4 @@ if (montoCompra > 100){
     console.log ("No aplica descuento para este monto.");
 }
 
-console.log ("El precio final a pagar es:$" + precioFinal)
+console.log ("El precio final a pagar es:$" + precioFinal);
